2d1e3fa03493b925a8030dd1e3959fc72e5c99b5
Josh Brearley <joshbrearley@live.com>
2020-03-30 09:46:24 +0100

    Updates

---
+++ precache-manifest.94327a3e0fc0b6d9c6f6e2adf8081847.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75d57f9e4adece449ed481dbd1f8139b",
+    "url": "/testsite/index.html"
+  },
+  {
+    "revision": "be44e9d6eb80f9ad8a15",
+    "url": "/testsite/static/css/2.ab965100.chunk.css"
+  },
+  {
+    "revision": "960774bf7e8edd16715a",
+    "url": "/testsite/static/css/main.72671706.chunk.css"
+  },
+  {
+    "revision": "be44e9d6eb80f9ad8a15",
+    "url": "/testsite/static/js/2.35756136.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/testsite/static/js/2.35756136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "960774bf7e8edd16715a",
+    "url": "/testsite/static/js/main.671f9404.chunk.js"
+  },
+  {
+    "revision": "19d436a1819ff8183d92",
+    "url": "/testsite/static/js/runtime-main.bac1d094.js"
+  }
+]);
